41e0478d4c58bcc2326c98db9baf8470b398f17d
Jason Devine <jdevine82@gmail.com>
2016-02-13 16:34:33 +1100

    removed all floats.. not needed. code for master and slave in theory...untested

--- modbusslave.ino/modbusslave.ino.ino
+++ modbusslave.ino/modbusslave.ino.ino
@@ -1,52 +1,62 @@
 #include <SimpleModbusSlave.h>
 #include <string.h>
 /* 
-   SimpleModbusSlaveV10 supports function 3, 6 & 16.
-   
-   This example code will receive the adc ch0 value from the arduino master. 
-   It will then use this value to adjust the brightness of the led on pin 9.
-   The value received from the master will be stored in address 1 in its own
-   address space namely holdingRegs[].
-   
-   In addition to this the slaves own adc ch0 value will be stored in 
-   address 0 in its own address space holdingRegs[] for the master to
-   be read. The master will use this value to alter the brightness of its
-   own led connected to pin 9.
-   
-   The modbus_update() method updates the holdingRegs register array and checks
-   communication.
+ This code is intended for a arduino nano. It uses the hardware serial port to connect to a master over a propietry hardware bus of open collector optocouplers. It uses the modbus protocol.
+ this does create some issues as the modbus protocol only uses 16bit registers so everyting is in mv. 
+ 2 cells are implemented on each node. 
+ reg      description
+ 0 Cell1Voltage,  //cell voltage in mv 
 
-   Note:  
-   The Arduino serial ring buffer is 64 bytes or 32 registers.
-   Most of the time you will connect the arduino to a master via serial
-   using a MAX485 or similar.
- 
-   In a function 3 request the master will attempt to read from your
-   slave and since 5 bytes is already used for ID, FUNCTION, NO OF BYTES
-   and two BYTES CRC the master can only request 58 bytes or 29 registers.
- 
-   In a function 16 request the master will attempt to write to your 
-   slave and since a 9 bytes is already used for ID, FUNCTION, ADDRESS, 
-   NO OF REGISTERS, NO OF BYTES and two BYTES CRC the master can only write
-   54 bytes or 27 registers.
- 
-   Using a USB to Serial converter the maximum bytes you can send is 
-   limited to its internal buffer which differs between manufactures. 
+ 1  Node1outputs, //this is a register for the dump loads. bit0 is cell output
+ 2 Node1TopSetpoint, //point that alarm turns on high bytes; used as float on slave in mv
+
+ 3 Node1BottomSetpoint,  /// point that low voltage alarm goes off low byte; used as float on slave in mv
+
+ 4 Node1HDumpSetpoint,   // point that dump resistor turn on High bytes. part of float in mv
+
+
+ The slave code needs to measure the hardware ADC, do some averaging and map into the modbus reg for reading by the master.
+ Also the voltages need to be compared to the floats given by the master. If the dump load needs turning on, or the cell volatage is too high or too low turn on the hardware alarm so that 
+ the inverter and charger will get turned off in the case if life batteries.
+
+ hardware connections
+ 0- TX
+ 1- RX
+ 2- reserved for tx enable if ever needed....
+ ADC0 lower cell  voltage read
+ ADC1 upper cell voltage read
+ 3- dump output for lower cell
+ 4- dump output for upper cell
+ 5- alarm output for high/low battery
 */
 
-#define  LED 13
 
+
+#define LowerCellDump 3 //hardware pin
+#define UpperCellDump 4 //hardware pin
+#define AlarmOutput 5 //hardware pin
 // Using the enum instruction allows for an easy method for adding and 
 // removing registers. Doing it this way saves you #defining the size 
 // of your slaves register array each time you want to add more registers
 // and at a glimpse informs you of your slaves register layout.
+
+unsigned int scratchpad1, scratchpad2;
 
 //////////////// registers of your slave ///////////////////
 enum 
 {     
   // just add or remove registers and your good to go...
   // The first register starts at address 0
-PWM_VAL,       
+ Cell1Voltage,  //voltage in mv
+  Cell1outputs, //this is a register for the dump loads. bit0 is lower cell, bit1 is upper cell.
+  Cell1TopSetpoint, //point that alarm turns on high bytes; used as float on slave in mv
+  Cell1BottomSetpoint,  /// point that low voltage alarm goes off low byte; used as float on slave in mv
+  Cell1DumpSetpoint,   // point that dump resistor turn on High bytes. part of float in mv    
+   Cell2Voltage,  //voltage in mv
+  Cell2outputs, //this is a register for the dump loads. bit0 is lower cell, bit1 is upper cell.
+  Cell2TopSetpoint, //point that alarm turns on high bytes; used as float on slave in mv
+  Cell2BottomSetpoint,  /// point that low voltage alarm goes off low byte; used as float on slave in mv
+  Cell2DumpSetpoint,   // point that dump resistor turn on High bytes. part of float in mv 
   HOLDING_REGS_SIZE // leave this one
   // total number of registers for function 3 and 16 share the same register array
   // i.e. the same address space
@@ -79,13 +89,15 @@ void setup()
      These byte formats are already defined in the Arduino global name space. 
   */
 	
-  modbus_configure(&Serial, 9600, SERIAL_8N2, 1, 10, HOLDING_REGS_SIZE, holdingRegs);
+  modbus_configure(&Serial, 9600, SERIAL_8N2, 0, 10, HOLDING_REGS_SIZE, holdingRegs);  //slave id will need to change some code to read hardware pins and set this
 
   // modbus_update_comms(baud, byteFormat, id) is not needed but allows for easy update of the
   // port variables and slave id dynamically in any function.
-  modbus_update_comms(9600, SERIAL_8N2, 1);
+  modbus_update_comms(9600, SERIAL_8N2, 0);   //slave id will need to change some code to read hardware pins and set this
   
-  pinMode(LED, OUTPUT);
+  pinMode(LowerCellDump, OUTPUT);
+  pinMode(UpperCellDump, OUTPUT);
+  pinMode(AlarmOutput, OUTPUT);
 }
 
 void loop()
@@ -95,36 +107,29 @@ void loop()
   // for fault finding by the modbus master.
   
   modbus_update();
-  
- // holdingRegs[ADC_VAL] = analogRead(A0); // update data to be read by the master to adjust the PWM
-  
-   analogWrite(LED, holdingRegs[PWM_VAL]>>2); // constrain adc value from the arduino master to 255
-  
-  /* Note:
-     The use of the enum instruction is not needed. You could set a maximum allowable
-     size for holdinRegs[] by defining HOLDING_REGS_SIZE using a constant and then access 
-     holdingRegs[] by "Index" addressing. 
-     I.e.
-     holdingRegs[0] = analogRead(A0);
-     analogWrite(LED, holdingRegs[1]/4);
-  */
-  
+  word cellvoltage=GetAveragePinReads(0);  //read and aveage voltage then put into register
+  if (cellvoltage>holdingRegs[Cell1TopSetpoint]) digitalWrite(AlarmOutput,HIGH);else digitalWrite(AlarmOutput,LOW); //turn on alarm for external shutdown
+   if (cellvoltage<holdingRegs[Cell1BottomSetpoint]) digitalWrite(AlarmOutput,HIGH);else digitalWrite(AlarmOutput,LOW); //turn on alarm for external shutdown
+     if (cellvoltage>holdingRegs[Cell1DumpSetpoint])  digitalWrite(LowerCellDump,HIGH); else digitalWrite(LowerCellDump,LOW); //turn on alarm for external shutdown
+holdingRegs[Cell1Voltage]=cellvoltage;  //put into register 
+ cellvoltage=GetAveragePinReads(1);  //read and aveage voltage then put into register
+  if (cellvoltage>holdingRegs[Cell2TopSetpoint]) digitalWrite(AlarmOutput,HIGH);else digitalWrite(AlarmOutput,LOW); //turn on alarm for external shutdown
+   if (cellvoltage<holdingRegs[Cell2BottomSetpoint]) digitalWrite(AlarmOutput,HIGH);else digitalWrite(AlarmOutput,LOW); //turn on alarm for external shutdown
+     if (cellvoltage>holdingRegs[Cell2DumpSetpoint]) digitalWrite(UpperCellDump,LOW); else digitalWrite(UpperCellDump,HIGH); //turn on alarm for external shutdown
+holdingRegs[Cell2Voltage]=cellvoltage;  //put into register 
+
 }
 
-
- 
-/**
- * Reform two 16bit unsigned integers that store one 32bit
- * signed float (little endian).
- * @param u1 the first 16bit unsigned
- * @param u2 the second 16bit unsigned
- * return the 32bit float
- */
-float reform_uint16_2_float32(uint16_t u1, uint16_t u2)
-{
-    long int num = ((u2 & 0xFFFF) << 16) | (u1 & 0xFFFF);
-    float numf;
-    memcpy(&numf, &num, 4);
-    return numf;
+//really need to change this so that it is done on a hardware timer interrupt and continueously running in background...
+ word GetAveragePinReads(int Sense){
+ int Ctr=0;
+unsigned long SumOfReads=0.0;
+  // get the prescribed number of samples
+while (Ctr < 1000){
+  SumOfReads = SumOfReads + analogRead(Sense);
+  Ctr++;}
+//Calculate and return the average in millivolts
+return word((SumOfReads/Ctr)/1.023*5);
 }
+
 
